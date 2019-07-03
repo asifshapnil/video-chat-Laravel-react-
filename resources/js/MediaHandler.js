366b508891bcf0b14ca86408d467a1542a5675f6
Asif Shapnil <asif.shapnil@gmail.com>
2019-07-03 15:56:47 +0600

    first commit

---
+++ resources/js/MediaHandler.js
@@ -0,0 +1,15 @@
+import { resolve } from "url";
+
+class MediaHandler {
+    getPermissions() {
+        return new Promise((res, rej) => {
+            navigator.mediaDevices.getUserMedia({video: true, audio: true})
+                .then((stream) => {
+                    resolve(stream);
+                })
+                .catch((err) => {
+                    throw new Error(`Unable to fetch stream${err}`);
+                });
+        });
+    }
+}
